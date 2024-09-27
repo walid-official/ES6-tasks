094014541382246646cd825ff6b8c1f445640247
walid hasan <walid.official8@gmail.com>
2024-09-28 01:52:09 +0600

    more tasks are added

--- index.js
+++ index.js
@@ -67,3 +67,103 @@ const marge = (marNum1,marNum2) => {
 }
 
 console.log(marge(margeNumber1,margeNumber2))
+
+
+
+
+// task-1 && option-1
+//********************
+
+const odd1 = [3,5,7,9,11];
+for(let i = 0; i < odd1.length; i++){
+    let sum = odd1[i] + 1;
+    // console.log(sum);
+}
+
+
+// task-1 && option-2
+//********************
+
+const odd2 = [3,5,7,9,11];
+for(let singleOdd of odd2){
+    // console.log(singleOdd + 1);
+}
+
+
+// task-1 && option-3
+//********************
+const odd3 = [3,5,7,9,11];
+// odd3.forEach(oddNum => console.log(oddNum + 1));
+
+
+
+// task-1 && option-4
+//********************
+const odd4 = [3,5,7,9,11];
+const result = odd4.map(oddMap => oddMap + 1);
+// console.log(result);
+// result.forEach(evenToOdd => console.log(evenToOdd - 1));
+
+
+
+
+
+//task-2
+//**********
+// const filterArray = [10,3,44,50,30,33,40];
+// const filterResult = filterArray.filter(num => num % 10 === 0 ? num : null);
+// console.log(filterResult);
+
+
+//task-3
+//**********
+
+// const instructor = [
+//     {name: "Nodi",age:28,position: "Senior"},
+//     {name: "Akil",age:38,position: "Junior"},
+//     {name: "Shobuj",age:30,position: "Senior"}
+// ]
+
+// const filterArray = instructor.filter(selectPosition => selectPosition.position.toLocaleLowerCase() === "senior")
+// const mapArray = filterArray.map(mapName => mapName.name)
+// console.log(mapArray);
+
+
+//task-4
+//**********
+
+// const people = [
+//     {name: "Nodi",age:28},
+//     {name: "Akil",age:38},
+//     {name: "Shobuj",age:30}
+// ]
+// sum = 0;
+// people.forEach(ageCalc => {
+//     sum += ageCalc.age; 
+// });
+// console.log(sum);
+
+
+// const people = [
+//     {name: "Nodi",age:28},
+//     {name: "Akil",age:38},
+//     {name: "Shobuj",age:30}
+// ]
+// const age = people.map(ageCalc => ageCalc.age);
+// const sum = age.reduce((ageStart,ageEnd) => ageStart + ageEnd,0);
+// console.log(sum);
+
+
+
+const people = [
+    {name: "Nodi",age:28},
+    {name: "Akil",age:38},
+    {name: "Shobuj",age:30}
+]
+const age = people.map(ageCalc => ageCalc.age);
+
+function myFunction(start,end){
+   return start + end;
+}
+
+console.log(age.reduce(myFunction));
